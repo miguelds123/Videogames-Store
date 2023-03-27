e6be8a5d5b7edc5f0cf6bcac34eaeea21c9edd80
Miguel <mdsantamaria02@gmail.com>
2023-03-26 18:50:17 -0600

    Agregue BLLListaDeseos

---
+++ VentaVideojuegos/Layers/BLL/BLLListaDeseos.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VentaVideojuegos
+{
+    class BLLListaDeseos : IBLLListaDeseos
+    {
+        public bool DeleteListaDeseos(string pIdCliente, string pIdProducto)
+        {
+            IDAListaDeseos _DALListaDeseos = new DALListaDeseos();
+
+            return _DALListaDeseos.DeleteListaDeseos(pIdCliente, pIdProducto);
+        }
+
+        public List<ListaDeseos> GetAllListaDeseos()
+        {
+            IDAListaDeseos _DALListaDeseos = new DALListaDeseos();
+
+            return _DALListaDeseos.GetAllListaDeseos();
+        }
+
+        public ListaDeseos GetListaDeseos(string pIdCliente, string pIdProducto)
+        {
+            IDAListaDeseos _DALListaDeseos = new DALListaDeseos();
+
+            return _DALListaDeseos.GetListaDeseos(pIdCliente, pIdProducto);
+        }
+
+        public List<ListaDeseos> GetListaDeseosByIdCliente(string pId)
+        {
+            IDAListaDeseos _DALListaDeseos = new DALListaDeseos();
+
+            return _DALListaDeseos.GetListaDeseosByIdCliente(pId);
+        }
+
+        public List<ListaDeseos> GetListaDeseosByIdProducto(string pId)
+        {
+            IDAListaDeseos _DALListaDeseos = new DALListaDeseos();
+
+            return _DALListaDeseos.GetListaDeseosByIdProducto(pId);
+        }
+
+        public ListaDeseos SaveListaDeseos(ListaDeseos pListaDeseos)
+        {
+            IDAListaDeseos _DALListaDeseos = new DALListaDeseos();
+
+            return _DALListaDeseos.SaveListaDeseos(pListaDeseos);
+        }
+
+        public ListaDeseos UpdateListaDeseos(ListaDeseos pListaDeseos)
+        {
+            IDAListaDeseos _DALListaDeseos = new DALListaDeseos();
+
+            return _DALListaDeseos.UpdateListaDeseos(pListaDeseos);
+        }
+    }
+}
