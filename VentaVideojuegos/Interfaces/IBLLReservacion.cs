d98ca20d3dea1ec955d78683e838a78780e45d32
Miguel <mdsantamaria02@gmail.com>
2023-03-26 18:57:22 -0600

    Agregue BLLReservacion

---
+++ VentaVideojuegos/Interfaces/IBLLReservacion.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VentaVideojuegos
+{
+    interface IBLLReservacion
+    {
+        Reservacion GetReservacionById(string pId);
+        List<Reservacion> GetReservacionByIdCliente(string pId);
+        List<Reservacion> GetAllReservacion();
+        Reservacion SaveReservacion(Reservacion pReservacion);
+        Reservacion UpdateReservacion(Reservacion pReservacion);
+        bool DeleteReservacion(string pId);
+    }
+}
---
+++ VentaVideojuegos/Layers/BLL/BLLReservacion.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VentaVideojuegos.Layers.BLL
+{
+    class BLLReservacion : IBLLReservacion
+    {
+        public bool DeleteReservacion(string pId)
+        {
+            IDALReservacion _DALReservacion = new DALReservacion();
+
+            return _DALReservacion.DeleteReservacion(pId);
+        }
+
+        public List<Reservacion> GetAllReservacion()
+        {
+            IDALReservacion _DALReservacion = new DALReservacion();
+
+            return _DALReservacion.GetAllReservacion();
+        }
+
+        public Reservacion GetReservacionById(string pId)
+        {
+            IDALReservacion _DALReservacion = new DALReservacion();
+
+            return _DALReservacion.GetReservacionById(pId);
+        }
+
+        public List<Reservacion> GetReservacionByIdCliente(string pId)
+        {
+            IDALReservacion _DALReservacion = new DALReservacion();
+
+            return _DALReservacion.GetReservacionByIdCliente(pId);
+        }
+
+        public Reservacion SaveReservacion(Reservacion pReservacion)
+        {
+            IDALReservacion _DALReservacion = new DALReservacion();
+
+            return _DALReservacion.SaveReservacion(pReservacion);
+        }
+
+        public Reservacion UpdateReservacion(Reservacion pReservacion)
+        {
+            IDALReservacion _DALReservacion = new DALReservacion();
+
+            return _DALReservacion.UpdateReservacion(pReservacion);
+        }
+    }
+}
