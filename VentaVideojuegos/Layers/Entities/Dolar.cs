8ff0b7e8526e621d135e9114f1d3b8bfb19b936c
Miguel <mdsantamaria02@gmail.com>
2023-04-06 22:00:31 -0600

    Termine ServiceBCCR

---
+++ VentaVideojuegos/Layers/Entities/Dolar.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VentaVideojuegos
+{
+    class Dolar
+    {
+        public string Codigo { get; set; }
+
+        public DateTime Fecha { get; set; }
+
+        public double Monto { get; set; }
+    }
+}
